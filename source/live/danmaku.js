7beb1b88a91ff326f28e3ece7fc1717b37b8e00c
bleatingsheep <bleatingsheep@live.cn>
2022-06-03 19:57:33 +0800

    add danmaku reconnection. hint user when sending fails.

--- source/live/danmaku.js
+++ source/live/danmaku.js
@@ -3,11 +3,21 @@ const liveDan = function (url, group, onMessage) {
     connection.onreconnected(function () {
         connection.invoke('JoinGroup', group).catch(err => console.error(err));
     });
-    connection.start().then(function () {
-        connection.invoke('JoinGroup', group).catch(err => console.error(err));
-    }).catch(err => console.error(err));
+    var tryStart = () => {
+        connection.start().then(function () {
+            connection.invoke('JoinGroup', group).catch(err => console.error(err));
+        }).catch(err => {
+            console.log({m: "Connection failed. Retrying in 5 seconds.", e: err});
+            window.setTimeout(tryStart, 5000);
+        });
+    };
+    tryStart();
     connection.on("ReceiveMessage", function (user, message) {
         onMessage(JSON.parse(message));
+    });
+    connection.onclose(t => {
+        console.log({m: "Connection closed. Reconnecting.", t});
+        tryStart();
     });
 
     return {
@@ -23,7 +33,10 @@ const liveDan = function (url, group, onMessage) {
                     window.localStorage.setItem("danmakuUserName", userName);
                 }
             }
-            connection.invoke('SendMessage', group, userName, JSON.stringify(mess)).catch(err => console.error(err));
+            connection.invoke('SendMessage', group, userName, JSON.stringify(mess)).catch(err => {
+                console.error(err);
+                window.alert("发送失败");
+            });
         },
         connection,
     };
